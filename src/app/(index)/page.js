6c5e9d6d5660d0d7930a54c1d664e8df124c40e2
Josh Kartadwirja <josh.kartadiwirja@kabinetpresident.gov.sr>
2024-09-05 09:18:12 -0300

    refactor: change file structure for homepage

--- src/app/(index)/page.js
+++ src/app/(index)/page.js
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Header2 from "./header2";
-// import Header from "./header";
 
 export default function Home() {
   return (
     <div className="relative">
       <Header2 />
-      {/* <Header /> */}
       <main className="flex h-[300vh] flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <p className="hidden fixed left-0 top-0 lg:flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
